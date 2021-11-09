b130366b371d8271d26224eca6aef0c2f59695c3
nhat150923 <nhatviptk003@gmail.com>
2021-11-09 15:31:43 +0700

    sửa đợt 1

---
+++ bai2.cpp
@@ -0,0 +1,16 @@
+#include<stdio.h>
+ int main()
+ {
+ 	int a;
+  printf("nhap mot so a=");
+  scanf("%d",&a);
+if(a>0)
+   printf("so do la so duong\n");
+ else
+ printf("so do la so am\n");
+if(a%2==0)
+   printf("so do la so chan\n");
+ else
+ printf("so do la so le\n");
+
+}
